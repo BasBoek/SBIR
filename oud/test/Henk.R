4228523fe5041760da28454b4c94bf24b81269b0
BasBoek <bastiaenboekelo@gmail.com>
2018-07-19 10:46:21 +0200

    Tamdamdam was stom. Ik heb het gebonjourd

--- oud/test/Henk.R
+++ oud/test/Henk.R
@@ -8,14 +8,6 @@ lijst <- c(1,5,238746,3)
 
 timed <- 0
 
-## Hoi, dit is een functie voor de cursus. Mooi. Sad...
-tamdamdam <- function(inputlijst, factor){
-  for(i in inputlijst){
-    timed <- i*factor  # tamdamdam
-    #assign("timed", timed, envir = .GlobalEnv) 
-  }
-  return(timed)
-}
 
 lijst[3]
 
